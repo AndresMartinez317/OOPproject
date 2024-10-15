301387fb1e2c7dd5fbfdebf140f47869962d1019
Andres Martinez <andres27oct@gmail.com>
2024-10-15 03:28:03 -0400

    second commit

--- src/main/java/org/example/projectoop/HelloController.java
+++ src/main/java/org/example/projectoop/HelloController.java
@@ -2,13 +2,27 @@ package org.example.projectoop;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.event.ActionEvent;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private TextField mark1txt;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField mark2txt;
+
+    @FXML
+    private TextField nametxt;
+
+    @FXML
+    private TextField totaltxt;
+    @FXML
+    void Submit(ActionEvent event) {
+        register();
+
+    }
+    public void register(){
+
     }
 }
